52fc81d95dd8b076734dcf514ed66142dd32542d
Ken Sodemann <ken@ionic.io>
2021-01-13 11:41:25 -0600

    cordova

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit {
   height = 0;
 
   constructor() {}
 
+  ngOnInit() {
+    window.addEventListener('keyboardWillShow', (event: any) => {
+      this.height = event.keyboardHeight;
+    });
+  }
 }
