64dfe3d94c044c7472d22e9e8572cb8be3e98c92
Ken Sodemann <ken@ionic.io>
2021-01-13 12:54:43 -0600

    capacitor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { KeyboardInfo, Plugins } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit {
   height = 0;
 
-  constructor() {}
+  constructor(private zone: NgZone) {}
 
+  ngOnInit() {
+    const { Keyboard } = Plugins;
+    Keyboard.addListener('keyboardWillShow', (info: KeyboardInfo) =>
+      this.zone.run(() => (this.height = info.keyboardHeight))
+    );
+  }
 }
